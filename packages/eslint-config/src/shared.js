9ea68948a9d5414ef74dbe041e3d1558f3113f9e
Jace Hensley <jacehensley@gmail.com>
2021-11-03 15:13:00 -0700

    change rules

--- packages/eslint-config/src/shared.js
+++ packages/eslint-config/src/shared.js
@@ -4,15 +4,20 @@ module.exports = {
   rules: {
     // Turn off rules
 
+    'prefer-destructuring': 'off',
     'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
     'no-restricted-syntax': 'off',
     'class-methods-use-this': 'off',
     '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
 
     // Configure rules
+
+    'no-void': ['error', { allowAsStatement: true }],
 
     '@typescript-eslint/naming-convention': [
       'error',
